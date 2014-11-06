d155329fdbb549863514e5c79198ca258922c2c1
Max Gfeller <max.gfeller@gmail.com>
2014-11-06 22:48:21 +0100

    Use `gify-parse` and `concat-stream` to get gif info

--- index.js
+++ index.js
@@ -1,28 +1,32 @@
 var fs = require('fs')
-var explode = require('gif-explode')
+var parse = require('gify-parse')
+var concat = require('concat-stream')
 
 module.exports = function(giffer) {
-    giffer.pre('saveMetaData', function(next, url, id) {
-        var filename = giffer.outDir + '/' + id + '.gif'
+  giffer.pre('saveMetaData', function(next, url, id) {
+    var filename = giffer.outDir + '/' + id + '.gif'
 
-        fs.stat(filename, function(err, stats) {
-            // when an error occured, drop the file
-            if(err) return
+    fs.stat(filename, function(err, stats) {
+      // when an error occured, drop the file
+      if(err) return
 
-            // if stats is null, the file is empty
-            if(!stats.size) return
+      // if stats is null, the file is empty
+      if(!stats.size) return
 
-            var frames = 0
-            var rs = fs.createReadStream(filename)
-                .pipe(explode(function(frame) {
-                    frames++
-                    if(frames === 2) {
-                        return next()
-                    }
-                })
-                .on('error', function() {
-                  // do nothing
-                }))
+      var frames = 0
+      var rs = fs.createReadStream(filename)
+        .pipe(concat(function(data) {
+          var i = parse.getInfo(data)
+
+          if(!i.valid) return
+
+          if(!i.animated) return
+
+          next()
+        }))
+        .on('error', function() {
+          return
         })
     })
+  })
 }
